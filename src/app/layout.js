e6027d77ad5a37808d72ea5aa0bbcd9317f0773a
srishylam burla <srishylamburla1@gmail.com>
2026-03-29 20:28:41 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,10 +26,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-       <link
+       {/* <link
           href="https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
-        />
+        /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
