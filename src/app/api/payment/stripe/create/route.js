0e1cbda8bf03505ac78693bec170d7ad48472454
srishylam burla <srishylamburla1@gmail.com>
2026-06-17 22:55:46 +0530

    fixed bugs

--- src/app/api/payment/stripe/create/route.js
+++ src/app/api/payment/stripe/create/route.js
@@ -1,133 +1,47 @@
 import stripe from '@/lib/stripe';
 import { NextResponse } from 'next/server';
-
 import { getAuthUser } from '@/lib/getAuthUser';
 
-
-export async function POST(req){
-
-const user =
-await getAuthUser();
-
-
-if(!user){
-
-return NextResponse.json(
-
-{
-success:false
-},
-
-{
-status:401
-}
-
-);
-
-}
-
-
-
-const {plan}=await req.json();
-
-
-
-const prices={
-
-basic:1200
-
-};
-
-
-
-
-const session=
-
-await stripe.checkout.sessions.create({
-
-
-payment_method_types:['card'],
-
-
-mode:'payment',
-
-
-
-line_items:[
-
-{
-
-price_data:{
-
-
-currency:'usd',
-
-
-
-product_data:{
-
-name:'NestMe Agent Plan'
-
-},
-
-
-
-unit_amount:prices[plan]
-
-
-},
-
-
-
-quantity:1
-
-}
-
-],
-
-
-
-
-metadata:{
-
-
-userId:user._id.toString(),
-
-
-plan
-
-
-},
-
-
-
-
-
-success_url:
-
-
-`${process.env.NEXT_PUBLIC_APP_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-
-
-
-
-cancel_url:
-
-
-`${process.env.NEXT_PUBLIC_APP_URL}/subscribe`
-
-
-});
-
-
-
-return NextResponse.json({
-
-url:session.url
-
-});
-
-
+export async function POST(req) {
+    const user = await getAuthUser();
+    if (!user) {
+        return NextResponse.json({
+            success: false
+        },
+            { status: 401 }
+        )
+    }
+    const { plan } = await req.json();
+
+    const prices = { basic: 1200 };
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: [{
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: 'NestMe Agent Plan'
+                },
+                unit_amount: prices[plan]
+            },
+            quantity: 1
+        }
+        ],
+        metadata: {
+            userId: user._id.toString(),
+            plan
+        },
+        success_url:
+            `${process.env.NEXT_PUBLIC_APP_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url:
+            `${process.env.NEXT_PUBLIC_APP_URL}/subscribe`
+    });
+
+    return NextResponse.json({
+        url: session.url
+    });
 }
 
 // import stripe from '@/lib/stripe';
--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,7 +1,12 @@
 import Stripe from 'stripe';
 
+console.log(
+    'STRIPE_SECRET_KEY:',
+    process.env.STRIPE_SECRET_KEY
+);
+
 const stripe = new Stripe(
-process.env.STRIPE_SECRET_KEY
+    process.env.STRIPE_SECRET_KEY
 );
 
 export default stripe;
