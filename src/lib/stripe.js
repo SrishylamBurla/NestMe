85d557bac2e6a6e0277a7a14fc2e08c955f49e79
srishylam burla <srishylamburla1@gmail.com>
2026-06-17 23:04:26 +0530

    fixed bugs

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,12 +1,10 @@
-import Stripe from 'stripe';
-
-console.log(
-    'STRIPE_SECRET_KEY:',
-    process.env.STRIPE_SECRET_KEY
-);
+import Stripe from "stripe";
 
 const stripe = new Stripe(
-    process.env.STRIPE_SECRET_KEY
+  process.env.STRIPE_SECRET_KEY,
+  {
+    apiVersion: "2025-05-28.basil",
+  }
 );
 
 export default stripe;
