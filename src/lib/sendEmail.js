1a099e4e3e679f54f246d83bf967fb0bc8052dc0
Srishylam Burla <srishylamburla1@gmail.com>
2026-07-26 19:55:57 +0530

    updated

--- src/lib/sendEmail.js
+++ src/lib/sendEmail.js
@@ -1,8 +1,6 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -12,13 +10,15 @@ console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
       },
     });
 
+    await transporter.verify();
+    console.log("SMTP Connected");
+    
     await transporter.sendMail({
       from: `"NestMe 🏡" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html,
     });
-
   } catch (error) {
     console.error("EMAIL ERROR:", error);
     throw new Error("Email failed to send");
