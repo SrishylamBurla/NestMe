323ec35724bfa6c99fdba41615812d3d036f5b0c
Srishylam Burla <srishylamburla1@gmail.com>
2026-07-27 00:37:13 +0530

    fixed bugs

--- src/app/api/admin/leads/route.js
+++ src/app/api/admin/leads/route.js
@@ -1,13 +1,21 @@
 import Lead from "@/models/Lead";
-import connectDB from "@/lib/db"
+import connectDB from "@/lib/db";
 import { NextResponse } from "next/server";
-import "@/models/AgentProfile"
-import "@/models/Property"
-import "@/models/User"
-
+import "@/models/AgentProfile";
+import "@/models/Property";
+import "@/models/User";
 
 export async function GET() {
   await connectDB();
-  const leads = await Lead.find().populate("property agent user");
+  const leads = await Lead.find()
+    .populate("property")
+    .populate("user", "name email")
+    .populate({
+      path: "agent",
+      populate: {
+        path: "user",
+        select: "name email phone",
+      },
+    });
   return NextResponse.json({ leads });
 }
