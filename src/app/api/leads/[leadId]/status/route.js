b4739f3fa7446f5b88b4c8623fbf4cfaf410ba6f
Srishylam Burla <srishylamburla1@gmail.com>
2026-07-30 16:45:09 +0530

    fixed-UI

--- src/app/api/leads/[leadId]/status/route.js
+++ src/app/api/leads/[leadId]/status/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import connectDB from "@/lib/db";
 import Lead from "@/models/Lead";
+import Property from "@/models/Property"
+import User from "@/models/Property"
 import { getAuthUser } from "@/lib/getAuthUser";
 
 export async function PUT(req, context) {
